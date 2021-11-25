0dfa8141b9a1a0464e39d1bd7119d5a65f5ca3be
Pierre Boisselier <pierre.boisselier@live.fr>
2021-11-25 12:24:42 +0100

    Added comment

--- weatherstation.cpp
+++ weatherstation.cpp
@@ -16,6 +16,13 @@ WeatherStation::WeatherStation(QWidget *parent)
         _currentWeather = Weather::Sunny;
 
         // Load images in memory
+        /*
+         * Should these images be loaded in RAM?
+         * This program is for a weather station and thus images will never have to be changed very frequently
+         * as the weather is not something that changes every second...
+         * But, as these images are very small and the RaspberryPi this thing will run on is plentiful of RAM
+         * we can all load them and make our job easier :)
+         */
         _images = {{Weather::Rainy, new QPixmap("assets/rainy.png")},
                    {Weather::Snowy, new QPixmap("assets/snowy.png")},
                    {Weather::Sunny, new QPixmap("assets/sunny.png")},
@@ -26,11 +33,11 @@ WeatherStation::WeatherStation(QWidget *parent)
 
 WeatherStation::~WeatherStation()
 {
-    // Delete loaded images
-    for (const auto &i : _images)
-    {
-        delete i.second;
-    }
+        // Delete loaded images
+        for (const auto &i : _images)
+        {
+                delete i.second;
+        }
 
         delete ui;
 }
@@ -39,63 +46,63 @@ WeatherStation::~WeatherStation()
 
 void WeatherStation::updateUI(double humidity, double temp, double pressure, double lux)
 {
-    _humidity = humidity;
-    _temp = temp;
+        _humidity = humidity;
+        _temp = temp;
         _pressure = pressure;
         _lux = lux;
 
-    // Change current weather
+        // Change current weather
         if (_lux < 4)
-            _currentWeather = Weather::Night;
+                _currentWeather = Weather::Night;
 
-       if (_pressure < 1015)
-       {
-           if (_humidity > 60)
-           {
-               if (_lux > 10 && _lux < 200)
-               {
-                   if (_temp > 5)
-                       _currentWeather = Weather::Rainy;
-               }
-               else if (_lux < 200)
-               {
-                   if (_temp < 5)
-                        _currentWeather = Weather::Snowy;
-               }
-           }
-       }
-       else if (_pressure <= 1015)
-       {
-           if (_humidity < 60)
-           {
-                if (_lux > 10 && _lux < 200)
-                    _currentWeather = Weather::SunnyCloud;
-           }
-           else
-           {
-               if (_lux > 4 && _lux <= 10)
-                   _currentWeather = Weather::Thunderstorm;
-           }
-       }
-       else
-       {
-           if (_humidity < 60)
-               if (_lux >= 200)
-                   _currentWeather = Weather::Sunny;
-       }
+        if (_pressure < 1015)
+        {
+                if (_humidity > 60)
+                {
+                        if (_lux > 10 && _lux < 200)
+                        {
+                                if (_temp > 5)
+                                        _currentWeather = Weather::Rainy;
+                        }
+                        else if (_lux < 200)
+                        {
+                                if (_temp < 5)
+                                        _currentWeather = Weather::Snowy;
+                        }
+                }
+        }
+        else if (_pressure <= 1015)
+        {
+                if (_humidity < 60)
+                {
+                        if (_lux > 10 && _lux < 200)
+                                _currentWeather = Weather::SunnyCloud;
+                }
+                else
+                {
+                        if (_lux > 4 && _lux <= 10)
+                                _currentWeather = Weather::Thunderstorm;
+                }
+        }
+        else
+        {
+                if (_humidity < 60)
+                        if (_lux >= 200)
+                                _currentWeather = Weather::Sunny;
+        }
 
-    refreshUI();
+        refreshUI();
 }
 
 void WeatherStation::refreshUI()
 {
-    // Refresh LCD numbers
-    ui->humidity_lcd->display(_humidity);
-    ui->temp_lcd->display(_temp);
-    ui->pressure_lcd->display(_pressure);
-    ui->lux_lcd->display(_lux);
+        // Refresh LCD numbers
+        ui->humidity_lcd->display(_humidity);
+        ui->temp_lcd->display(_temp);
+        ui->pressure_lcd->display(_pressure);
+        ui->lux_lcd->display(_lux);
 
-    // Change image
-    ui->weatherView->setPixmap(*_images[_currentWeather]);
-    ui->weatherView->show();
+        // Change image
+        ui->weatherView->setPixmap(*_images[_currentWeather]);
+        ui->weatherView->show();
 }
